3fd862280ea6558e1dacdafb9155b8ab2016bef0
rasupy <lyukey0902@gmail.com>
2025-08-05 12:00:38 +0900

    refactor(file): unify add related script into a single file

--- static/js/add_category.js
+++
@@ -1,9 +0,0 @@
-// カテゴリー追加モーダル制御
-document.addEventListener("DOMContentLoaded", function () {
-    const categoryModal = document.getElementById("category-modal");
-    const createCategoryBtn = document.getElementById("create-category-btn");
-    const closeCategoryBtn = document.getElementById("close-category-modal");
-
-    createCategoryBtn.addEventListener("click", () => categoryModal.classList.remove("hidden"));
-    closeCategoryBtn.addEventListener("click", () => categoryModal.classList.add("hidden"));
-});
